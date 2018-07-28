be1c5daa5ab142d4651d299730b6980d0226479d
Kenneth Bogner <kenneth.bogner@assurant.com>
2018-07-27 23:43:29 -0400

    :sparkles: create player model
      The `Hnefatafl.Player` model will be the main model for player data
      later on. Only the `name` field exists now just to provide a way to
      differentiate players.

---
+++ priv/repo/migrations/20180727024529_create_players.exs
@@ -0,0 +1,12 @@
+defmodule Hnefatafl.Repo.Migrations.CreatePlayers do
+  use Ecto.Migration
+
+  def change do
+    create table(:players) do
+      add :name, :string
+
+      timestamps()
+    end
+
+  end
+end
